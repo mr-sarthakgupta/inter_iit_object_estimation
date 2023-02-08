b00ee2165d55c685455a2b122efb21c63c32154e
Sarthak Gupta <81774392+mr-sarthakgupta@users.noreply.github.com>
2023-02-08 21:42:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route("/scanner", methods=['POST'])
 def get_volumetric_weight():
-    nparr1 = np.fromstring(request.data.file1, np.uint8)
-    nparr2 = np.fromstring(request.data.file2, np.uint8)
+    nparr1 = np.fromstring(request.data["images"][0], np.uint8)
+    nparr2 = np.fromstring(request.data["images"][1], np.uint8)
     img1 = cv2.imdecode(nparr1, cv2.IMREAD_COLOR)
     img2 = cv2.imdecode(nparr1, cv2.IMREAD_COLOR)
     return integrate_results(get_dims_from_one_img_1(img1), get_dims_from_one_img_2(img2))
