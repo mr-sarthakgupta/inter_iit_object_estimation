4d47e1cc18388361b9ce09e62c75a4772846bbb2
Sarthak Gupta <81774392+mr-sarthakgupta@users.noreply.github.com>
2023-02-08 19:21:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify
-from controllers.dims_from_one_img import get_dims_from_one_img
+import numpy as np
+import cv2
+from flask import Flask, request
+from controllers.dims_from_one_img import get_dims_from_one_img_1, get_dims_from_one_img_2, integrate_results
 
 app = Flask(__name__)
 
 
-@app.route("/scanner/<string:img_path>", methods=['GET', 'POST'])
-def get_volumetric_weight(img_path):
-    return get_dims_from_one_img(img_path)
+@app.route("/scanner", methods=['POST'])
+def get_volumetric_weight():
+    nparr1 = np.fromstring(request.data.file1, np.uint8)
+    nparr2 = np.fromstring(request.data.file2, np.uint8)
+    img1 = cv2.imdecode(nparr1, cv2.IMREAD_COLOR)
+    img2 = cv2.imdecode(nparr1, cv2.IMREAD_COLOR)
+    return integrate_results(get_dims_from_one_img_1(img1), get_dims_from_one_img_2(img2))
