b7489b84a94b330923eeb67c8982d935c7f3789c
Sarthak Gupta <81774392+mr-sarthakgupta@users.noreply.github.com>
2023-02-10 01:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 import numpy as np
 import cv2
-from flask import Flask, request
+from flask import Flask, request, json
+import base64
+import io
 from controllers.dims_from_one_img import get_dims_from_one_img_1, get_dims_from_one_img_2, integrate_results
 
 app = Flask(__name__)
 
-
 @app.route("/scanner", methods=['POST'])
 def get_volumetric_weight():
-    nparr1 = np.fromstring(request.data["images"][0], np.uint8)
-    nparr2 = np.fromstring(request.data["images"][1], np.uint8)
-    img1 = cv2.imdecode(nparr1, cv2.IMREAD_COLOR)
-    img2 = cv2.imdecode(nparr1, cv2.IMREAD_COLOR)
-    return integrate_results(get_dims_from_one_img_1(img1), get_dims_from_one_img_2(img2))
+    uploaded_file = open('sample.json')
+    uploaded_file = json.load(uploaded_file)
+    x1 = base64.b64decode(uploaded_file['images'][0][1:])
+    img_1 = open('decoded_1.png', 'wb')
+    img_1.write(x1)
+    x2 = base64.b64decode(uploaded_file['images'][1][1:])
+    img_2 = open('decoded_2.png', 'wb')
+    img_2.write(x2)
+    return integrate_results(get_dims_from_one_img_1('decoded_1.png'), get_dims_from_one_img_2('decoded_2.png'))
